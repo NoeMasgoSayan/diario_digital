15c95de091bfceee52a01c80fd796c630d8b693a
Noelia <masgonoelia21@gmail.com>
2024-10-01 20:28:14 -0500

    navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Importamos la app
+import { app } from "./firebase.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAE4bAnGPuCqyLLmK7zCOvxHR057TcmjN0",
-  authDomain: "diario-digital-1c065.firebaseapp.com",
-  projectId: "diario-digital-1c065",
-  storageBucket: "diario-digital-1c065.appspot.com",
-  messagingSenderId: "423812313530",
-  appId: "1:423812313530:web:7a14e7b99cc31685586d83"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+console.log(app);
